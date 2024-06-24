48257e1ef322701aa8dd1fc93086fc97809975ae
kmkim0713 <kmkimworkwork@gmail.com>
2024-06-24 23:40:18 +0900

    순수 자바 코드로 주문  로직 작성해보기

---
+++ src/test/java/com/example/springcore/order/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package com.example.springcore.order;
+
+import com.example.springcore.member.Grade;
+import com.example.springcore.member.Member;
+import com.example.springcore.member.MemberService;
+import com.example.springcore.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+
+        // 기본형은 Null을 넣을 수 없다
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId,"itemA", 10000);
+
+        // assertj 를 사용
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
