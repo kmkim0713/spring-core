95a532cbd7403d0c8fe99f6f1b3d609dd91d4229
kmkim0713 <kmkimworkwork@gmail.com>
2024-06-25 17:24:53 +0900

    테스트 코드도 AppConfig를 사용하여 주입받도록 관심사 분리 적용

--- src/main/java/com/example/springcore/AppConfig.java
+++ src/main/java/com/example/springcore/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.springcore;
 
+import com.example.springcore.discount.DiscountPolicy;
 import com.example.springcore.discount.FixDiscountPolicy;
 import com.example.springcore.member.MemberService;
 import com.example.springcore.member.MemberServiceImpl;
@@ -12,13 +13,20 @@ public class AppConfig {
     // 생성자 주입
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    // cmd + option + m 으로 (new 객체())를 get 함수로 만들 수 있음
+    private MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
     }
 
-
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 }
